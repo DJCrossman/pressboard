c03eb58dab756274801de21464ed6b5af47b2984
DJCrossman <david@crossman.io>
2019-12-10 18:08:06 -0600

    feat: Added base functionality for documenting
    - Added piece to read `pressboard.conf.json`
    - Use Puppeteer to visit each route
    - Add screenshots and README.md documents

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const puppeteer = require('puppeteer');
+
+(async () => {
+  try {
+    let content = []
+    let config = {}
+    let configPath = __dirname + '/pressboard.conf.json'
+    config = await new Promise((resolve) => {
+      fs.readFile(configPath, 'utf8', (err, data) => {
+        if (err) throw err
+        resolve(JSON.parse(data))
+      });
+    })
+
+    if (!config.routes) {
+      throw new Error(`👎 Must have routes in config.`)
+    }
+    if (!config.url) {
+      throw new Error(`👎 Must have routes in config.`)
+    }
+    await Promise.all(config.routes.map(async (route, index) => {
+      let args = []
+      if(!!route.igconito) { args.push('--incognito') }
+      let browser = await puppeteer.launch({headless: true, args, defaultViewport: {width: 1920, height: 1080} });
+      let page = await browser.newPage();
+      let baseUrl = route.url || config.url
+      if (route.params && typeof route.params === 'object') {
+        Object.keys(route.params).forEach((key) => route.path = route.path.replace(`:${key}`, route.params[key]))
+      }
+      let name = route.name || route.path.replace('/', '-')
+      try {
+        await page.goto(`${baseUrl}/#/${route.path}`);
+        if(!!route.selector) await page.waitForSelector(route.selector)
+        await page.screenshot({path: `./docs/imgs/${name}.png`});
+        let title = await page.title()
+        await browser.close();
+        content.push({
+          index,
+          title,
+          path: `./imgs/${name}.png`
+        })
+      } catch (e) {
+        console.log(`👎 Failed to load "${baseUrl}/#/${route.path}"`)
+      }
+    }))
+    fs.writeFileSync(
+      __dirname + "/docs/README.md",
+      content.sort((a,b) => a.index - b.index)
+             .map((c) => `# ${c.title} \n ![${c.title}](${c.path})`).join('\n\n')
+    )
+    console.log('🎉 Successfully added documentation')
+    process.exit()
+  } catch (e) {
+    console.trace(e)
+    console.error(e)
+    process.exit(1)
+  }
+})();
